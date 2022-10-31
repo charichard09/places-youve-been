26fc4e11858d23d2a0a8013b60df7c12ea95b7cf
Richard <charichard09@gmail.com>
2022-10-31 15:59:38 -0700

    BUGFIX:add event listener load
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -25,30 +25,35 @@ function Destination(location, timeOfYear, landmark, notes) {
 }
 
 // UI Logic
-let travelLog = new TravelLog();
-let destination = new Destination("Iceland", "Summer 2019", "Vatnajokull", "Amazing!");
-travelLog.addDestination(destination);
-let destination2 = new Destination("Japan", "May 2013", "Tokyo", "Kind place");
-travelLog.addDestination(destination2);
+function doThisOnLoad() {
+  let travelLog = new TravelLog();
+  let destination = new Destination("Iceland", "Summer 2019", "Vatnajokull", "Amazing!");
+  travelLog.addDestination(destination);
+  let destination2 = new Destination("Japan", "May 2013", "Tokyo", "Kind place");
+  travelLog.addDestination(destination2);
 
-let li = document.getElementById("li-id");
-li.innerText = travelLog.destinations["1"].location;
+  // let li = document.getElementById("li-id");
+  // li.innerText = travelLog.destinations["1"].location;
 
 
-//First Trial:
-// Display names of individual destinations in our TravelLog object 
-// destination === [["location", "Japan"], ["timeOfYear", "May 2013"]...]
-// Object.keys(travelLog.destinations).forEach( function (destination) {
-//   let locationLi = document.createElement("li");
-//   let descriptionLi = document.createElement("li");
-
-//   // locationLi.append(travelLog.destinations["1"].location);
-//   locationLi.append("Hi");
-//   // locationLi.setAttribute("id", "location-li");
-//   // descriptionLi.append(travelLog.destinations["1"].timeOfYear, travelLog.destinations["1"].landmark, travelLog.destinations["1"].notes);
-//   descriptionLi.append("World");
-//   console.log(travelLog.destinations["1"].timeOfYear, travelLog.destinations["1"].landmark, travelLog.destinations["1"].notes)
-//   document.getElementById("append").append(locationLi);
-// // });
+  //First Trial:
+  // Display names of individual destinations in our TravelLog object 
+  // destination === [["location", "Japan"], ["timeOfYear", "May 2013"]...]
+  Object.keys(travelLog.destinations).forEach( function (destination) {
+    let locationLi = document.createElement("li");
+    let descriptionLi = document.createElement("li");
+  
+    locationLi.append(travelLog.destinations[destination].location);
+    locationLi.setAttribute("id", "location-li");
+    descriptionLi.append(travelLog.destinations[destination].timeOfYear, 
+    travelLog.destinations[destination].landmark, travelLog.destinations[destination].notes);
+    document.getElementById("append").append(locationLi);
+    document.getElementById("append").append(descriptionLi);
+  });
+}
 
 // // When a user clicks on a place's name, display properties of the individual destinations
+
+window.addEventListener("load", function() {
+  doThisOnLoad();
+});
